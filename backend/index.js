cd3f21c5232d4f0d193067680c930db92fb10994
kumargourav999 <116900816+kumargourav999@users.noreply.github.com>
2025-08-28 21:01:35 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const app=express();
 const db=require('./db')
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 3000;
 const cors=require("cors");
 const path = require('path');
 const bodyparder=require("body-parser")
