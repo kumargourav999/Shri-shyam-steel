34ab361f33d52bbcae068fae3864243436efd1f9
kumargourav999 <116900816+kumargourav999@users.noreply.github.com>
2025-08-30 21:25:25 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose');
-mongoose.connect("mongodb://localhost:27017/ShriShyamSteel").
+mongoose.connect("mongodb+srv://kumargourav:kumar123@cluster0.dxvytbg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").
 then(()=>{
     console.log("database connected successfully");
 }).catch((err)=>{
